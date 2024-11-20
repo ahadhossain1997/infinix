5aa117fa3607649f85beb78f87d25dc4fcd5f38d
ahad hossain <wpahad1997@gmail.com>
2024-11-20 21:45:07 +0600

    infinix-third page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,5 @@
 "use strict";
 
-function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
-function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : String(i); }
-function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
 function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
@@ -136,6 +132,51 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
         mainClass: 'mfp-fade'
       });
     }
+    var brand_slider = $('.infinix-brand-slider-wrap');
+    if (brand_slider.is_exist()) {
+      brand_slider.slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 4000,
+        arrows: false,
+        pauseOnHover: false,
+        cssEase: 'linear',
+        responsive: [{
+          breakpoint: 1599,
+          settings: {
+            slidesToShow: 5
+          }
+        }, {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 4
+          }
+        }, {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3
+          }
+        }, {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 3
+          }
+        }, {
+          breakpoint: 700,
+          settings: {
+            slidesToShow: 2
+          }
+        }, {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 1
+          }
+        }]
+      });
+    }
+
     /*--------------------------------------------------------------
     INFINIX COUNTER JS INIT
     --------------------------------------------------------------*/
@@ -247,94 +288,45 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
         }]
       });
     }
+    /*--------------------------------------------------------------
+    INFINIX  TEAM SLIDER JS INIT
+    --------------------------------------------------------------*/
 
-    /*--------------------------------------------------------------
-    infinix THREE COLUMN SLIDER JS INIT
-    --------------------------------------------------------------*/
-    var p_slider = $('.infinix-three-column');
-    if (p_slider.is_exist()) {
-      p_slider.slick(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty({
-        slidesToShow: 3,
+    var team_slider = $('.infinix-team-slider');
+    if (team_slider.is_exist()) {
+      team_slider.slick({
+        slidesToShow: 4,
         slidesToScroll: 1,
+        arrows: false,
         autoplay: false,
-        arrows: true
-      }, "slidesToScroll", 1), "infinite", true), "speed", 1000), "centerMode", true), "lazyLoad", 'progressive'), "prevArrow", '<button class="slide-arrow infinix-p-slider-next"></button>'), "nextArrow", '<button class="slide-arrow infinix-p-slider-prev"></button>'), "responsive", [{
-        breakpoint: 1399,
-        settings: {
-          slidesToShow: 2
-        }
-      }, {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 2,
-          centerPadding: '70px'
-        }
-      }, {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          centerPadding: '0px'
-        }
-      }])).slickAnimation();
-    }
-    /*--------------------------------------------------------------
-    infinix BRAND SLIDER INIT
-    --------------------------------------------------------------*/
-    var s_slider = $('.infinix-three-column2');
-    if (s_slider.is_exist()) {
-      s_slider.slick(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: false,
-        arrows: true
-      }, "slidesToScroll", 1), "infinite", true), "speed", 1000), "lazyLoad", 'progressive'), "prevArrow", '<button class="slide-arrow infinix-s-slider-next"></button>'), "nextArrow", '<button class="slide-arrow infinix-s-slider-prev"></button>'), "responsive", [{
-        breakpoint: 1399,
-        settings: {
-          slidesToShow: 2
-        }
-      }, {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 2,
-          centerPadding: '70px'
-        }
-      }, {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          centerPadding: '0px'
-        }
-      }]));
-    }
-
-    /*--------------------------------------------------------------
-    infinix TSTIMONIAL SLIDER INIT
-    --------------------------------------------------------------*/
-    var t_slider = $('.infinix-t-slider-init');
-    if (t_slider.is_exist()) {
-      t_slider.slick(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: false,
-        arrows: true
-      }, "slidesToScroll", 1), "infinite", true), "speed", 1000), "lazyLoad", 'progressive'), "prevArrow", '<button class="slide-arrow infinix-t-slider-init-next"></button>'), "nextArrow", '<button class="slide-arrow infinix-t-slider-init-prev"></button>'), "responsive", [{
-        breakpoint: 1399,
-        settings: {
-          slidesToShow: 2
-        }
-      }, {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 2,
-          centerPadding: '70px'
-        }
-      }, {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          centerPadding: '0px'
-        }
-      }]));
+        dots: true,
+        centerMode: true,
+        centerPadding: '200px',
+        lazyLoad: 'progressive',
+        responsive: [{
+          breakpoint: 1349,
+          settings: {
+            slidesToShow: 2
+          }
+        }, {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 1
+          }
+        }, {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            centerPadding: '100px'
+          }
+        }, {
+          breakpoint: 575,
+          settings: {
+            slidesToShow: 1,
+            centerPadding: '0px'
+          }
+        }]
+      });
     }
 
     /*--------------------------------------------------------------
