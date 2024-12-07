184b200a9747d4ac05efcf5b65aaf2e362261b5a
ahad hossain <wpahad1997@gmail.com>
2024-12-07 23:50:36 +0600

    last finish

--- assets/js/app.js
+++ assets/js/app.js
@@ -641,3 +641,37 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
     });
   }
 })(jQuery);
+$(function () {
+  var link = $('#scroll-btn a.luminix-default-btn.faq-btn');
+
+  // Move to specific section when click on menu link
+  link.on('click', function (e) {
+    var target = $($(this).attr('href'));
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 600);
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
+  // Run the scrNav when scroll
+  $(window).on('scroll', function () {
+    scrNav();
+  });
+
+  // scrNav function 
+  // Change active dot according to the active section in the window
+  function scrNav() {
+    var sTop = $(window).scrollTop();
+    $('section').each(function () {
+      var id = $(this).attr('id'),
+        offset = $(this).offset().top - 1,
+        height = $(this).height();
+      if (sTop >= offset && sTop < offset + height) {
+        link.removeClass('active');
+        $('#scroll-btn').find('[data-scroll="' + id + '"]').addClass('active');
+      }
+    });
+  }
+  scrNav();
+});
